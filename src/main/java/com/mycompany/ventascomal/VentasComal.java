96de5d369a2fecd40ae3e280340a2171be080ac6
salvadorsin123 <gemsal22@gmail.com>
2023-12-06 13:08:29 -0600

    Firts

---
+++ src/main/java/com/mycompany/ventascomal/VentasComal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ventascomal;
+
+/**
+ *
+ * @author Salvador
+ */
+public class VentasComal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
